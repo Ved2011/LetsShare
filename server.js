2a5bf15d54ba59514b42f0f00fec46c96cbd6175
Ved <monsterved2011@outlook.com>
2026-05-11 19:21:04 +0530

    Update server.js

--- server.js
+++ server.js
@@ -16,6 +16,11 @@ const useragent = require('express-useragent');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+
 // Middleware
 app.use(cors());
 app.use(express.json());
